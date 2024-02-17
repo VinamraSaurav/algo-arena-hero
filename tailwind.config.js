c3d1155e17ec96e309e848f9948580e987d067c4
Akangkha Sarkar <akangkhasarkar@gmail.com>
2024-02-18 01:47:19 +0530

    added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "color-1": "#E1C3FF",
+      "color-2": "#9D5AE3",
+      "color-3": "#663B9C",
+      "color-4": "#3C3D8E",
+      "color-5": "#3D306F",
+      "color-6": "#161A33",
+      primary: "#0F1629",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+      poppins: ["Poppins", "sans-serif"],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
