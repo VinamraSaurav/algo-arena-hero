49aceb267960b0e94f41083261c7d837341b38d0
Akangkha Sarkar <akangkhasarkar@gmail.com>
2024-02-18 14:37:38 +0530

    Updated the ui of navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
 
 import "./globals.css";
-
+import Navbar from "@/component/Navbar";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      <Navbar />
+      {children}</body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,29 +6,30 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      "color-1": "#E1C3FF",
-      "color-2": "#9D5AE3",
-      "color-3": "#663B9C",
-      "color-4": "#3C3D8E",
-      "color-5": "#3D306F",
-      "color-6": "#161A33",
-      primary: "#0F1629",
-    },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-      poppins: ["Poppins", "sans-serif"],
-      the_bold_font:['The Bold Font']
-    },
     extend: {
+      colors: {
+        "color-1": "#E1C3FF",
+        "color-2": "#9D5AE3",
+        "color-3": "#663B9C",
+        "color-4": "#3C3D8E",
+        "color-5": "#3D306F",
+        "color-6": "#161A33",
+        primary: "#0F1629",
+      },
+      fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+        poppins: ["Poppins", "sans-serif"],
+        the_bold_font: ["The Bold Font"],
+      },
       backgroundImage: {
-     
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-          "gradient-horizon":"linear-gradient(90deg, #FFF 2.09%, #9D5AE3 75.49%)",
-          "gradient-hero-img":"radial-gradient(50% 50% at 50% 50%, rgba(128, 116, 255, 0.67) 0%, rgba(123, 112, 238, 0.00) 100%)"
+        "gradient-horizon":
+          "linear-gradient(90deg, #FFF 2.09%, #9D5AE3 75.49%)",
+        "gradient-hero-img":
+          "radial-gradient(50% 50% at 50% 50%, rgba(128, 116, 255, 0.67) 0%, rgba(123, 112, 238, 0.00) 100%)",
       },
     },
   },
